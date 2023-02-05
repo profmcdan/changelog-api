84ff082436589f8e3e2bc89f68caf4cdee2d65ed
Daniel Ale <danielale9291@gmail.com>
2023-02-05 23:00:16 +0100

    feat: add login and register

--- dist/middleware/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.customMiddleware = void 0;
-var customMiddleware = function (req, res, next) {
-    req.requestTime = Date.now();
-    next();
-};
-exports.customMiddleware = customMiddleware;
-//# sourceMappingURL=index.js.map
